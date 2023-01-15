6d6e305c6bd9edbcaa7574c461c4b34ca991a5a5
csz-ecust <1091029242@qq.com>
2023-01-15 12:53:10 +0800

    change

--- chapter-01/recipe-01/cxx-example/hello-world.cpp
+++ chapter-01/recipe-01/cxx-example/hello-world.cpp
@@ -6,5 +6,7 @@ std::string say_hello() { return std::string("Hello, CMake world!"); }
 
 int main() {
   std::cout << say_hello() << std::endl;
+  std::cout << "cmake" << std::endl;
+  std::cout << "try" << std::endl;
   return EXIT_SUCCESS;
 }
